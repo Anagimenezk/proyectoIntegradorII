850db1f4eb88c1bbf2d1038637fc6a6d7fb0b509
Anagimenezk <gimeneza@udesa.edu.ar>
2021-04-23 20:14:16 -0300

    terminando for y agregando detalles

--- data/productos.js
+++ data/productos.js
@@ -31,7 +31,7 @@ let productos = {
             id: 2,
             nombre: "Chila",
             fecha: 2020/03/22,
-            descripcion: "Este moderno bistró, que abre para el desayuno, el almuerzo y la cena, se convirtió rápidamente en un éxito por su forma única de transformar los sabores argentinos familiares. ",
+            descripcion: "La cocina de este restaurante es en igual medidas coqueto y vanguardista. Está comandada por Pedro Bargero. ",
             imagen: "/images/products/chila.jpeg",
             comentarios: [
                 {
@@ -83,7 +83,7 @@ let productos = {
             id: 4,
             nombre: "Don Julio",
             fecha: 2020/02/23,
-            descripcion: "Este moderno bistró, que abre para el desayuno, el almuerzo y la cena, se convirtió rápidamente en un éxito por su forma única de transformar los sabores argentinos familiares. ",
+            descripcion: "No es fácil tomar algo tan cotidiano y popular como es la carne a la parrilla, y llevarlo a un nivel de excelencia sin precedentes y no caer en la pretensión o sobre exigencia, pero este restaurant, lo logro.",
             imagen: "/images/products/donjulio.jpg",
             comentarios: [
                 {
@@ -109,23 +109,27 @@ let productos = {
             id: 5,
             nombre: "Elena",
             fecha: 2020/03/02,
-            descripcion: "Este moderno bistró, que abre para el desayuno, el almuerzo y la cena, se convirtió rápidamente en un éxito por su forma única de transformar los sabores argentinos familiares. ",
+            descripcion: "Elena es probablemente el lugar donde más sofisticado te sientas comiendo un bife de chorizo en Buenos Aires. ",
             imagen: "/images/products/elena.jpg",
             comentarios: [
                 {
+                    id: 1,
                     usuario: 'Ana Gimenez' ,
                     Comentario: 'Muy buen restaurante, volveria mil veces mas.',
                 },
 
                 {
+                    id:2,
                     usuario: 'Luisa Bengolea' ,
                     Comentario: 'Muy buena ambientacion y servicio',
                 },
                 {
+                    id: 3,
                     usuario: 'Martina Pawluk' ,
                     Comentario: 'La comida muy rica, la atencion excelente',
                 },
                 {
+                    id:4,
                     usuario: 'Cala Ruggeri' ,
                     Comentario: 'De los mejores resturantes que fui en esta cuidad, felicitaciones',
                 }
@@ -135,7 +139,7 @@ let productos = {
             id: 6,
             nombre: "Mishiguene",
             fecha: 2020/06/20,
-            descripcion: "Este moderno bistró, que abre para el desayuno, el almuerzo y la cena, se convirtió rápidamente en un éxito por su forma única de transformar los sabores argentinos familiares. ",
+            descripcion: "Todos los platos (además de deliciosos) tienen una historia que contar, y en general, esta se relaciona a la vida de Tomas Kalika, el cocinero, y sus raíces judías. . ",
             imagen: "/images/products/mishiguene.jpg",
             comentarios: [
                 {
@@ -161,7 +165,7 @@ let productos = {
             id: 7,
             nombre: "Mudra Plant Based",
             fecha: 2020/06/12,
-            descripcion: "Este moderno bistró, que abre para el desayuno, el almuerzo y la cena, se convirtió rápidamente en un éxito por su forma única de transformar los sabores argentinos familiares. ",
+            descripcion: "Son pocos los restaurantes que logran colarse en las conversaciones gastronómicas cuando todavía no abrieron sus puertas, Mudrá es una de esas excepciones. ",
             imagen: "/images/products/mudra.jpg",
             comentarios: [
                 {
@@ -187,7 +191,7 @@ let productos = {
             id: 8,
             nombre: "Narda Comedor",
             fecha: 2020/08/18,
-            descripcion: "Este moderno bistró, que abre para el desayuno, el almuerzo y la cena, se convirtió rápidamente en un éxito por su forma única de transformar los sabores argentinos familiares. ",
+            descripcion: "La atención es cálida y eficiente, los platos son para compartir y el único sello que tienen es el de lo impredecible: tanto en sabores como en origen. ",
             imagen: "/images/products/narda.jpg",
             comentarios: [
                 {
@@ -213,7 +217,7 @@ let productos = {
             id: 9,
             nombre: "Proper",
             fecha: 2020/06/10,
-            descripcion: "Este moderno bistró, que abre para el desayuno, el almuerzo y la cena, se convirtió rápidamente en un éxito por su forma única de transformar los sabores argentinos familiares. ",
+            descripcion: "De afuera este garage sobre calle Araoz poco te va a llevar a un restaurante en esta lista, pero todo eso se borra cuando probás el primer plato. ",
             imagen: "/images/products/proper.jpeg",
             comentarios: [
                 {
@@ -239,8 +243,60 @@ let productos = {
             id: 10,
             nombre: "Tegui",
             fecha: 2020/09/28,
-            descripcion: "Este moderno bistró, que abre para el desayuno, el almuerzo y la cena, se convirtió rápidamente en un éxito por su forma única de transformar los sabores argentinos familiares. ",
+            descripcion: "Con un precio fijo, es una experiencias gastronómicas por pasos pionera en la utilización de producto local de altísima calidad. Todos los pasos están maridados por vinos especialmente seleccionados.  ",
             imagen: "/images/products/tegui.jpg",
+            comentarios: [
+                {
+                    usuario: 'Ana Gimenez' ,
+                    Comentario: 'Muy buen restaurante, volveria mil veces mas.',
+                },
+
+                {
+                    usuario: 'Luisa Bengolea' ,
+                    Comentario: 'Muy buena ambientacion y servicio',
+                },
+                {
+                    usuario: 'Martina Pawluk' ,
+                    Comentario: 'La comida muy rica, la atencion excelente',
+                },
+                {
+                    usuario: 'Cala Ruggeri' ,
+                    Comentario: 'De los mejores resturantes que fui en esta cuidad, felicitaciones',
+                }
+            ]
+        },
+        {
+            id: 11,
+            nombre: "Sacro",
+            fecha: 2020/10/07,
+            descripcion: "La elegante arquitectura de Sacro, atrae a la multitud vegana y carnívora en busca de platos creativos y sabrosos a base de plantas. El menú de inspiración internacional fue diseñado por el chef ejecutivo Maximiliano Rossi. ",
+            imagen: "/images/products/sacro.jpeg",
+            comentarios: [
+                {
+                    usuario: 'Ana Gimenez' ,
+                    Comentario: 'Muy buen restaurante, volveria mil veces mas.',
+                },
+
+                {
+                    usuario: 'Luisa Bengolea' ,
+                    Comentario: 'Muy buena ambientacion y servicio',
+                },
+                {
+                    usuario: 'Martina Pawluk' ,
+                    Comentario: 'La comida muy rica, la atencion excelente',
+                },
+                {
+                    usuario: 'Cala Ruggeri' ,
+                    Comentario: 'De los mejores resturantes que fui en esta cuidad, felicitaciones',
+                }
+            ]
+        },
+        {
+            id: 12,
+            nombre: "Casa Cavia",
+            fecha: 2020/12/28,
+            descripcion: "El exclusivo restaurante judío del chef Tomás Kalika recrea platos asquenazíes, sefardíes, israelíes y del Medio Oriente utilizando ingredientes frescos y técnicas modernas de una manera que intriga y evoca nostalgia. ",
+            imagen: "/images/products/casacavia.jpeg",
             comentarios: [
                 {
                     usuario: 'Ana Gimenez' ,
