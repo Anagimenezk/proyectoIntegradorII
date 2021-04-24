6b7c62dd286769d61ded4f77a996efd827809585
Anagimenezk <gimeneza@udesa.edu.ar>
2021-04-24 12:04:30 -0300

    agregue idUsuario

--- data/productos.js
+++ data/productos.js
@@ -6,6 +6,7 @@ let productos = {
             fecha: 2020/03/02,
             descripcion: "Este moderno bistró, que abre para el desayuno, el almuerzo y la cena, se convirtió rápidamente en un éxito por su forma única de transformar los sabores argentinos familiares. ",
             imagen: "/images/products/alos.jpg",
+            idUsario: 1,
             comentarios: [
                 {
                     usuario: 'Ana Gimenez' ,
@@ -33,6 +34,7 @@ let productos = {
             fecha: 2020/03/22,
             descripcion: "La cocina de este restaurante es en igual medidas coqueto y vanguardista. Está comandada por Pedro Bargero. ",
             imagen: "/images/products/chila.jpeg",
+            idUsario: 3,
             comentarios: [
                 {
                     usuario: 'Ana Gimenez' ,
@@ -59,6 +61,7 @@ let productos = {
             fecha: 2020/05/14,
             descripcion: "Tal vez uno de los restaurantes de la lista más difíciles de encasillar. La comida tiene mucha influencia de platos y street food asiático, pero no es un restaurante asiático. Se siente bastante argentino.",
             imagen: "/images/products/dabbang.jpg",
+            idUsario: 2,
             comentarios: [
                 {
                     usuario: 'Ana Gimenez' ,
@@ -85,6 +88,7 @@ let productos = {
             fecha: 2020/02/23,
             descripcion: "No es fácil tomar algo tan cotidiano y popular como es la carne a la parrilla, y llevarlo a un nivel de excelencia sin precedentes y no caer en la pretensión o sobre exigencia, pero este restaurant, lo logro.",
             imagen: "/images/products/donjulio.jpg",
+            idUsario: 4,
             comentarios: [
                 {
                     usuario: 'Ana Gimenez' ,
@@ -111,6 +115,7 @@ let productos = {
             fecha: 2020/03/02,
             descripcion: "Elena es probablemente el lugar donde más sofisticado te sientas comiendo un bife de chorizo en Buenos Aires. ",
             imagen: "/images/products/elena.jpg",
+            idUsario: 3,
             comentarios: [
                 {
                     id: 1,
@@ -141,6 +146,7 @@ let productos = {
             fecha: 2020/06/20,
             descripcion: "Todos los platos (además de deliciosos) tienen una historia que contar, y en general, esta se relaciona a la vida de Tomas Kalika, el cocinero, y sus raíces judías. . ",
             imagen: "/images/products/mishiguene.jpg",
+            idUsario: 5,
             comentarios: [
                 {
                     usuario: 'Ana Gimenez' ,
@@ -167,6 +173,7 @@ let productos = {
             fecha: 2020/06/12,
             descripcion: "Son pocos los restaurantes que logran colarse en las conversaciones gastronómicas cuando todavía no abrieron sus puertas, Mudrá es una de esas excepciones. ",
             imagen: "/images/products/mudra.jpg",
+            idUsario: 1,
             comentarios: [
                 {
                     usuario: 'Ana Gimenez' ,
@@ -193,6 +200,7 @@ let productos = {
             fecha: 2020/08/18,
             descripcion: "La atención es cálida y eficiente, los platos son para compartir y el único sello que tienen es el de lo impredecible: tanto en sabores como en origen. ",
             imagen: "/images/products/narda.jpg",
+            idUsario: 4,
             comentarios: [
                 {
                     usuario: 'Ana Gimenez' ,
@@ -219,6 +227,7 @@ let productos = {
             fecha: 2020/06/10,
             descripcion: "De afuera este garage sobre calle Araoz poco te va a llevar a un restaurante en esta lista, pero todo eso se borra cuando probás el primer plato. ",
             imagen: "/images/products/proper.jpeg",
+            idUsario: 2,
             comentarios: [
                 {
                     usuario: 'Ana Gimenez' ,
@@ -245,6 +254,7 @@ let productos = {
             fecha: 2020/09/28,
             descripcion: "Con un precio fijo, es una experiencias gastronómicas por pasos pionera en la utilización de producto local de altísima calidad. Todos los pasos están maridados por vinos especialmente seleccionados.  ",
             imagen: "/images/products/tegui.jpg",
+            idUsario: 5,
             comentarios: [
                 {
                     usuario: 'Ana Gimenez' ,
@@ -271,6 +281,7 @@ let productos = {
             fecha: 2020/10/07,
             descripcion: "La elegante arquitectura de Sacro, atrae a la multitud vegana y carnívora en busca de platos creativos y sabrosos a base de plantas. El menú de inspiración internacional fue diseñado por el chef ejecutivo Maximiliano Rossi. ",
             imagen: "/images/products/sacro.jpeg",
+            idUsario: 1,
             comentarios: [
                 {
                     usuario: 'Ana Gimenez' ,
@@ -297,6 +308,7 @@ let productos = {
             fecha: 2020/12/28,
             descripcion: "El exclusivo restaurante judío del chef Tomás Kalika recrea platos asquenazíes, sefardíes, israelíes y del Medio Oriente utilizando ingredientes frescos y técnicas modernas de una manera que intriga y evoca nostalgia. ",
             imagen: "/images/products/casacavia.jpeg",
+            idUsario: 3,
             comentarios: [
                 {
                     usuario: 'Ana Gimenez' ,
