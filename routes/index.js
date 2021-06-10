d840c4d1c37914410d3942b383a3a6bc12dd80be
Martina <mat.u.ap@hotmail.com>
2021-06-10 10:38:22 -0300

    cambios multer

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,17 @@ let indexController = require ('../controllers/indexController');
 const multer = require ('multer');
 const path = require ('path')
 
-const multerRegister = multer.diskStorage({
-    destination: (req, file, cb) => {
-      let direccion = 'public/images/users';
-      cb(null, direccion);
-    },
-    filename: (req, file, cb) => {
-      let nombreArchivo = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
-      cb(null, nombreArchivo);
-    }
-  });
-  
-  const upload = multer({  storage: multerRegister});
 
-  const multerEdit = multer.diskStorage({
+
+router.get ('/', indexController.index);
+
+router.get ('/product/:id', indexController.product);
+
+router.get ('/search-results', indexController.search);
+
+   // router.get('/buscar',indexController.listar);
+
+   const multerEdit = multer.diskStorage({ 
     destination: (req, file, cb) => {
       let direccion = 'public/images/users';
       cb(null, direccion);
@@ -32,30 +29,9 @@ const multerRegister = multer.diskStorage({
 
   const uploadEdit = multer({  storage: multerEdit});
 
-  const multerAdd = multer.diskStorage({
-    destination: (req, file, cb) => {
-      let direccion = 'public/images/products';
-      cb(null, direccion);
-    },
-    filename: (req, file, cb) => {
-      let nombreArchivo = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
-      cb(null, nombreArchivo);
-    }
-  });
-
-  const uploadAdd = multer({  storage: multerAdd});
-
-router.get ('/', indexController.index);
-
-router.get ('/product/:id', indexController.product);
-
-router.get ('/search-results', indexController.search);
-
-   // router.get('/buscar',indexController.listar);
-
 router.get ('/profile/:id', indexController.profile);
 
-router.get ('/profile-edit',upload.single('edit_image'), indexController.profiledit);
+router.get ('/profile-edit',uploadEdit.single('edit_image'), indexController.profiledit);
 
 router.get ('/allProducts', indexController.allProducts);
 
@@ -66,15 +42,40 @@ router.get ('/allProducts', indexController.allProducts);
 //router.get('/borrar', indexController.borrarProducto);
 
 
+const multerAdd = multer.diskStorage({
+  destination: (req, file, cb) => {
+    let direccion = 'public/images/products';
+    cb(null, direccion);
+  },
+  filename: (req, file, cb) => {
+    let nombreArchivo = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
+    cb(null, nombreArchivo);
+  }
+});
+
+const uploadAdd = multer({  storage: multerAdd});
+
 router.get ('/add', indexController.productadd);
-    router.post('/add',uploadAdd.single('image'), indexController.crear);
+    router.post('/add',uploadAdd.single('productImage'), indexController.crear);
     
 
 router.get ('/login', indexController.login);
 router.post('/login', indexController.loginUsuario);
 
+const multerRegister = multer.diskStorage({
+  destination: (req, file, cb) => {
+    let direccion = 'public/images/users';
+    cb(null, direccion);
+  },
+  filename: (req, file, cb) => {
+    let nombreArchivo = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
+    cb(null, nombreArchivo);
+  }
+});
+const upload = multer({  storage: multerRegister});
+
 router.get ('/register', indexController.register);
-router.post('/register', uploadEdit.single('productImage') , indexController.crearUsuario);
+router.post('/register', upload.single('image') , indexController.crearUsuario);
 
 router.get('/logout', indexController.logout);
 
